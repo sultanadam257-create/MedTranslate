fd11660094df5f99af40044e6d041b4bd55c4856
sultanadam257-create <sultanadam257@gmail.com>
2026-09-24 21:14:31 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "MedTranslate",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v15)],
     products: [.executable(name: "MedTranslate", targets: ["MedTranslate"])],
     targets: [.executableTarget(name: "MedTranslate", path: "MedTranslate/Sources/MedTranslate")]
 )
